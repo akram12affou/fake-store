2a4416d905f971c7cae4a819de34aaa19a4372a6
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-01-27 22:09:11 +0100

    'some-changes'

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import '../styles/ProductCart.css'
+import InfoIcon from '@mui/icons-material/Info';
 function ProductCart({product}) {
    
-    const {id,title,description,image,price} = product;
+    const {id,title,description,category,image,price} = product;
     // console.log(id,title,description,image,price)
   return (
     
         <main class='container' key={id}>
             <div className='img'>
+            
+                <center>
+                Category :<i>{category}</i>
             <img src={image} alt="" />
+            </center>
             </div>
             <div className='details'>
             <h2>{title}</h2>
-            <p>{description}</p>
+            {/* <p>{description}</p> */}
+            
             <h3>Price : {price} $</h3>
+            <div class="info">
+            <InfoIcon/><a href="">Details</a>
+            </div>
             </div>
         </main>
    
--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ProductCart from './ProductCart'
+import '../styles/store.css'
 function Store({products}) {
-  
+   console.log(products)
   // console.log(products.filter((e => e.category =="men's clothing")))
   return (
-    <div>
+    <div >
+    Categorie :
+    <div class='container'>
       {products.map((product=> {
         return(
           <ProductCart product={product}/>
         )
       }))}
+    </div>
     </div>
   )
 }
