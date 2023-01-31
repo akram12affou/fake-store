62ddf6a15c6bd654dde5178d397b97c5dcf0150a
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2023-01-31 02:24:14 +0100

    'some-changes-in-shopping-cart'

--- src/components/ShoppingC.jsx
+++ src/components/ShoppingC.jsx
@@ -7,6 +7,7 @@ import "../styles/ShoppingC.css";
 function ShoppingC() {
   const dispatch = useDispatch();
   
+  const [minustotal, setMinustotal] = useState(0);
   const [products, setProducts] = useState([]);
   const [total, setTotal] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -53,8 +54,18 @@ function ShoppingC() {
     for (let i = 0; total.length > i; i++) {
       sum = total[i][0] * total[i][1] + sum;
     }
-    return sum.toFixed(2);
+    return sum;
   };
+  
+  const handleCancel = ( id,number) => {
+    const product = products.filter(e => e.id == id)
+    let minustotale = product[0].price*number + minustotal
+    setMinustotal(minustotale)
+    const newproducts = products.filter(e => e.id !== id)
+     setProducts(newproducts)
+     dispatch({type:'cancel_shopping_cart',payload:id})
+  }
+  console.log(minustotal)
   return (
     <div class='shopping-cart'>
       {products.length == 0 && loading == false ? (
@@ -74,6 +85,7 @@ function ShoppingC() {
                   <p>{e.title}</p>
                   <p class='quantity'>quantity : {number(e.id)}</p>
                 </div>
+                <div class='cancel' onClick={() =>handleCancel(e.id,number(e.id))}>X</div>
               </div>
             );
           })}
@@ -83,7 +95,7 @@ function ShoppingC() {
       )}
       <div class='Total-Buy-con'> 
      <div class='Total-Buy'>
-      {!loading && <p>Total : {totalsum()} $</p>}
+      {!loading && <p>Total : {(totalsum()-minustotal).toFixed(2)} $</p>}
       {products.length !== 0 && <button onClick={handleBuy}>buy</button>}
       </div>
       </div>
